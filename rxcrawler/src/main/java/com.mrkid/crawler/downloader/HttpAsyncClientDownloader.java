3b8bb1519c40aad021251055cc02ff07ffcc94f2
wuxudong <wuxudong@wandoujia.comxudong82113@gmail.com>
2016-12-10 11:40:41 +0800

    fix  Downloader  toFlowable(CompletableFuture<T> future)

--- rxcrawler/src/main/java/com.mrkid/crawler/downloader/HttpAsyncClientDownloader.java
+++ rxcrawler/src/main/java/com.mrkid/crawler/downloader/HttpAsyncClientDownloader.java
@@ -4,6 +4,7 @@ import com.mrkid.crawler.CrawlerException;
 import com.mrkid.crawler.Page;
 import com.mrkid.crawler.Request;
 import io.reactivex.Flowable;
+import io.reactivex.processors.BehaviorProcessor;
 import lombok.Data;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
@@ -19,6 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
@@ -36,78 +38,83 @@ public class HttpAsyncClientDownloader implements Downloader {
     private final CloseableHttpAsyncClient client;
 
     private static <T> Flowable<T> toFlowable(CompletableFuture<T> future) {
-        return Flowable.defer(() -> emitter ->
-                future.whenComplete((result, error) -> {
-                    if (error != null) {
-                        emitter.onError(error);
-                    } else {
-                        emitter.onNext(result);
-                        emitter.onComplete();
-                    }
-                }));
+        final BehaviorProcessor<T> processor = BehaviorProcessor.create();
+
+        future.whenComplete((result, error) -> {
+            if (error != null) {
+                processor.onError(error);
+            } else {
+                processor.onNext(result);
+                processor.onComplete();
+            }
+        });
+
+        return processor;
     }
 
 
     @Override
     public Flowable<Page> download(Request request) {
 
-        return Flowable.defer(() -> {
-            CompletableFuture<Page> promise = new CompletableFuture<>();
+        CompletableFuture<Page> promise = new CompletableFuture<>();
 
-            HttpUriRequest httpUriRequest = null;
-            switch (request.getMethod()) {
-                case "GET":
-                    httpUriRequest = new HttpGet(request.getUrl());
-                    break;
-                case "POST":
-                    final HttpPost httpPost = new HttpPost(request.getUrl());
-                    final List<BasicNameValuePair> nameValuePairs = request.getForm().entrySet().stream()
-                            .map(e -> new BasicNameValuePair(e.getKey(), e.getValue()))
-                            .collect(Collectors.toList());
+        HttpUriRequest httpUriRequest = null;
+        switch (request.getMethod()) {
+            case "GET":
+                httpUriRequest = new HttpGet(request.getUrl());
+                break;
+            case "POST":
+                final HttpPost httpPost = new HttpPost(request.getUrl());
+                final List<BasicNameValuePair> nameValuePairs = request.getForm().entrySet().stream()
+                        .map(e -> new BasicNameValuePair(e.getKey(), e.getValue()))
+                        .collect(Collectors.toList());
 
+                try {
                     httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "utf-8"));
-                    httpUriRequest = httpPost;
-                    break;
+                } catch (UnsupportedEncodingException e) {
+                    return Flowable.error(e);
+                }
+                httpUriRequest = httpPost;
+                break;
 
-                default:
-                    throw new UnsupportedOperationException(request.getMethod() + " is not supported");
+            default:
+                throw new UnsupportedOperationException(request.getMethod() + " is not supported");
+        }
+
+        logger.info("download {} method {} form {}", request.getUrl(), request.getMethod(), request.getForm());
+        client.execute(httpUriRequest, new FutureCallback<HttpResponse>() {
+            @Override
+            public void completed(HttpResponse httpResponse) {
+                final int status = httpResponse.getStatusLine().getStatusCode();
+                if (status != HttpStatus.SC_OK) {
+                    promise.completeExceptionally(new CrawlerException(request.getUrl() + " return " + status));
+                } else {
+                    try {
+                        final String value = IOUtils.toString(httpResponse.getEntity().getContent(), "utf-8");
+
+                        Page page = new Page(request);
+                        page.setRawText(value);
+
+                        promise.complete(page);
+                    } catch (IOException e) {
+                        promise.completeExceptionally(new CrawlerException(e));
+                    }
+                }
+
             }
 
-            logger.info("download {} method {} form {}", request.getUrl(), request.getMethod(), request.getForm());
-            client.execute(httpUriRequest, new FutureCallback<HttpResponse>() {
-                @Override
-                public void completed(HttpResponse httpResponse) {
-                    final int status = httpResponse.getStatusLine().getStatusCode();
-                    if (status != HttpStatus.SC_OK) {
-                        promise.completeExceptionally(new CrawlerException(request.getUrl() + " return " + status));
-                    } else {
-                        try {
-                            final String value = IOUtils.toString(httpResponse.getEntity().getContent(), "utf-8");
+            @Override
+            public void failed(Exception e) {
+                promise.completeExceptionally(new CrawlerException(e));
+            }
 
-                            Page page = new Page(request);
-                            page.setRawText(value);
-
-                            promise.complete(page);
-                        } catch (IOException e) {
-                            promise.completeExceptionally(new CrawlerException(e));
-                        }
-                    }
-
-                }
-
-                @Override
-                public void failed(Exception e) {
-                    promise.completeExceptionally(new CrawlerException(e));
-                }
-
-                @Override
-                public void cancelled() {
-                    promise.cancel(false);
-                }
-            });
-
-            return toFlowable(promise);
+            @Override
+            public void cancelled() {
+                promise.cancel(false);
+            }
         });
+
+        return toFlowable(promise);
 
 
     }
